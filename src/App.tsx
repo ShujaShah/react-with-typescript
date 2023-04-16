00b615464be4ebac13101498f4fef25fa952a30c
ShujaShah <shuja.ashraf11@gmail.com>
2023-04-17 00:33:49 +0530

    exercises

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,78 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import Cart from "./components/Cart";
 
-function App() {
-  const [cart, setCart] = useState(["Product1", "Product 2", "Product 3"]);
+// function App() {
+//   const [cart, setCart] = useState(["Product1", "Product 2", "Product 3"]);
 
-  const submit = () => {
-    setCart([]);
+//   const submit = () => {
+//     setCart([]);
+//   };
+
+//   return (
+//     <>
+//       <Navbar cartCount={cart.length} />
+//       <Cart cartItems={cart} handleClick={submit} />
+//     </>
+//   );
+// }
+
+// object Properties Updation exercise
+// function App() {
+//   const [game, setGame] = useState({
+//     id: 1,
+//     player: {
+//       name: "shuja",
+//     },
+//   });
+
+//   const updatedPlayer = () => {
+//     setGame({ ...game, player: { name: "Shuja Shah" } });
+//   };
+
+//   return (
+//     <>
+//       <button onClick={updatedPlayer}>Update</button>
+//     </>
+//   );
+// }
+
+// Updating the array
+// function App() {
+//   const [pizza, setPizza] = useState({
+//     name: "Spicy Pepporoni",
+//     toppings: ["Mushroom"],
+//   });
+
+//   const updatedPizza = () => {
+//     setPizza({ ...pizza, toppings: ["Cheese"] });
+//   };
+
+//   return (
+//     <>
+//       <button onClick={updatedPizza}>Update</button>
+//     </>
+//   );
+// }
+
+function App() {
+  const [cart, setCart] = useState({
+    discount: 1,
+    items: [
+      { id: 1, title: "Product 1", quantity: 1 },
+      { id: 2, title: "Product 2", quantity: 1 },
+    ],
+  });
+
+  const updatedCart = () => {
+    setCart({
+      ...cart,
+      items: cart.items.map((item) => (item.id === 1 ? { ...item, quantity: item.quantity + 1 } : item)),
+    });
   };
 
   return (
     <>
-      <Navbar cartCount={cart.length} />
-      <Cart cartItems={cart} handleClick={submit} />
+      <button onClick={updatedCart}>Update</button>
     </>
   );
 }
