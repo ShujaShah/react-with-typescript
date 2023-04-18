64a43c1d06637568acfbcf9b325db1920ba1a27f
ShujaShah <shuja.ashraf11@gmail.com>
2023-04-18 18:37:30 +0530

    updating the values using ref hook

--- src/App.tsx
+++ src/App.tsx
@@ -3,97 +3,12 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import Cart from "./components/Cart";
 import ExpandableTextArea from "./components/ExpandableTextArea";
+import Form from "./components/Form";
 
-// function App() {
-//   const [cart, setCart] = useState(["Product1", "Product 2", "Product 3"]);
-
-//   const submit = () => {
-//     setCart([]);
-//   };
-
-//   return (
-//     <>
-//       <Navbar cartCount={cart.length} />
-//       <Cart cartItems={cart} handleClick={submit} />
-//     </>
-//   );
-// }
-
-// object Properties Updation exercise
-// function App() {
-//   const [game, setGame] = useState({
-//     id: 1,
-//     player: {
-//       name: "shuja",
-//     },
-//   });
-
-//   const updatedPlayer = () => {
-//     setGame({ ...game, player: { name: "Shuja Shah" } });
-//   };
-
-//   return (
-//     <>
-//       <button onClick={updatedPlayer}>Update</button>
-//     </>
-//   );
-// }
-
-// Updating the array
-// function App() {
-//   const [pizza, setPizza] = useState({
-//     name: "Spicy Pepporoni",
-//     toppings: ["Mushroom"],
-//   });
-
-//   const updatedPizza = () => {
-//     setPizza({ ...pizza, toppings: ["Cheese"] });
-//   };
-
-//   return (
-//     <>
-//       <button onClick={updatedPizza}>Update</button>
-//     </>
-//   );
-// }
-
-// function App() {
-//   const [cart, setCart] = useState({
-//     discount: 1,
-//     items: [
-//       { id: 1, title: "Product 1", quantity: 1 },
-//       { id: 2, title: "Product 2", quantity: 1 },
-//     ],
-//   });
-
-//   // here we need to update the product 1's quantity after clicking on the button
-//   const updatedCart = () => {
-//     setCart({
-//       ...cart,
-//       items: cart.items.map((item) => (item.id === 1 ? { ...item, quantity: item.quantity + 1 } : item)),
-//     });
-//   };
-
-//   return (
-//     <>
-//       <button onClick={updatedCart}>Update</button>
-//     </>
-//   );
-// }
-
-//Expandable Text Area Example
 function App() {
   return (
     <>
-      <ExpandableTextArea maxChars={100}>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique harum exercitationem nam illo. Accusamus
-        officia sed ad natus quae hic, saepe aperiam nostrum veniam laudantium, est, veritatis fuga iusto voluptates
-        blanditiis pariatur ex soluta corporis vero! Quae similique possimus quam dolorem sequi, natus ducimus tempore
-        rem impedit mollitia odio facilis, molestiae quod. Et accusantium quod nostrum voluptatum sunt neque
-        consequuntur alias facilis veritatis architecto ratione, quaerat praesentium dolor delectus velit unde nobis
-        ipsum tempora dicta! Delectus veniam id atque soluta expedita. Quas fugiat nobis consequatur assumenda
-        voluptatem quaerat molestias. Voluptatibus, maxime sint labore tempore hic adipisci ipsum vero sapiente quod.
-      </ExpandableTextArea>
+      <Form />
     </>
   );
 }
---
+++ src/components/Form.tsx
@@ -0,0 +1,35 @@
+import React, { FormEvent, useRef } from "react";
+const Form = () => {
+  const nameRef = useRef<HTMLInputElement>(null);
+  const ageRef = useRef<HTMLInputElement>(null);
+
+  const person = { name: "", age: 0 };
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    if (nameRef.current !== null) person.name = nameRef.current.value;
+    if (ageRef.current !== null) person.age = parseInt(ageRef.current.value);
+    console.log(person);
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="mb-3">
+        <label htmlFor="name" className="form-label">
+          Name
+        </label>
+        <input ref={nameRef} id="name" type="text" className="form-control" />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="age" className="form-label">
+          Age
+        </label>
+        <input ref={ageRef} id="age" type="number" className="form-control" />
+      </div>
+      <button type="submit" className="btn btn-primary">
+        Submit
+      </button>
+    </form>
+  );
+};
+
+export default Form;
