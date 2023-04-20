ed6909257ea9e78c79bc8a34c54ac398a901d88b
ShujaShah <shuja.ashraf11@gmail.com>
2023-04-20 23:27:47 +0530

    completed the form component

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -3,8 +3,19 @@ import { useForm, FieldValues } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 
+const schema = z.object({
+  description: z.string().min(3, { message: "Description must be atleast 5 characters" }),
+  amount: z.number({ invalid_type_error: "Amount is Required" }).min(1, { message: "Amount cannot be less than 1" }),
+  category: z.string({ invalid_type_error: "Category is Required" }),
+});
+type FormData = z.infer<typeof schema>;
+
 const Form = () => {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm<FormData>({ resolver: zodResolver(schema) });
 
   const onSubmit = (data: FieldValues) => {
     console.log(data);
@@ -16,22 +27,25 @@ const Form = () => {
           Description
         </label>
         <input {...register("description")} id="description" type="text" className="form-control" />
+        {errors.description && <p className="text-danger">{errors.description.message}</p>}
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           Amount
         </label>
-        <input {...register("amount")} id="amount" type="number" className="form-control" />
+        <input {...register("amount", { valueAsNumber: true })} id="amount" type="number" className="form-control" />
+        {errors.amount && <p className="text-danger">{errors.amount.message}</p>}
       </div>
       <div className="mb-3">
-        <select className="form-select" {...register("category")} aria-label="Default select example">
-          <option selected>Open this select menu</option>
-          <option value="1">Groceries</option>
-          <option value="2">Utilities</option>
-          <option value="3">Entertainment</option>
+        <select className="form-select" {...register("category")} id="category" aria-label="Please Choose an option">
+          <option selected>Please Choose an option</option>
+          <option value="Groceries">Groceries</option>
+          <option value="Utilities">Utilities</option>
+          <option value="Entertainment">Entertainment</option>
         </select>
+        {errors.category && <p className="text-danger">{errors.category.message}</p>}
       </div>
-      <button type="submit" className="btn btn-primary">
+      <button disabled={!isValid} type="submit" className="btn btn-primary">
         Submit
       </button>
     </form>
