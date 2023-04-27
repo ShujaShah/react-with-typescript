51a5898a3e3d7092f2e7a729759871024d9214df
ShujaShah <shuja.ashraf11@gmail.com>
2023-04-27 11:15:32 +0530

    added a loading spinner

--- src/App.tsx
+++ src/App.tsx
@@ -12,18 +12,26 @@ interface User {
 function App() {
   const [users, setUsers] = useState<User[]>([]);
   const [error, setError] = useState("");
+  const [isloading, setLoading] = useState(false);
   useEffect(() => {
     const controller = new AbortController();
+    setLoading(true);
     axios
       .get<User[]>("https://jsonplaceholder.typicode.com/users", { signal: controller.signal })
-      .then((res) => setUsers(res.data))
+      .then((res) => {
+        setUsers(res.data);
+        setLoading(false);
+      })
       .catch((err) => {
-        if(err instanceof CanceledError) return;
-        setError(err.message);});
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+        setLoading(false);
+      });
     return () => controller.abort();
   }, []);
   return (
     <>
+      {isloading && <div className="spinner-border"> </div>}
       {error && <p className="text-danger">{error}</p>}
       <ul>
         {users.map((user) => (
