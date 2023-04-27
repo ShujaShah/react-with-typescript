560005dad06278ecb25509ee63619a37c1ccca41
ShujaShah <shuja.ashraf11@gmail.com>
2023-04-27 10:58:46 +0530

    errorHandling using async await

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import ProductList from "./components/ProductList";
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 
 interface User {
   id: number;
@@ -13,10 +13,17 @@ function App() {
   const [users, setUsers] = useState<User[]>([]);
   const [error, setError] = useState("");
   useEffect(() => {
-    axios
-      .get<User[]>("https://jsonplaceholder.typicode.com/users")
-      .then((res) => setUsers(res.data))
-      .catch((err) => setError(err.message));
+    const fetchUsers = async () => {
+      try {
+        const res = await axios.get<User[]>("https://jsonplaceholder.typicode.com/users");
+        setUsers(res.data);
+      } catch (err) {
+        setError((err as AxiosError).message);
+      }
+    };
+    fetchUsers();
+    // .then((res) => setUsers(res.data))
+    // .catch((err) => setError(err.message));
   }, []);
   return (
     <>
